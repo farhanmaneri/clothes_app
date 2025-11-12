3e8754708e7924b8a72b64ba13db3eb0f1bb57c4
farhanmaneri <farhanmaneri@gmail.com>
2025-11-12 22:05:51 +0500

    changes made to upload backend on vercel

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,21 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads")); // serve images
+app.use("/uploads", express.static("uploads"));
 app.use("/api/products", productRoutes);
 
-// Connect DB
-connectDB();
-app.get("/", (req, res) => {
+// Default route for testing
+app.get("/api", (req, res) => {
   res.send("Backend is running");
 });
-// ✅ Vercel expects a function export, not app.listen()
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+// Connect DB
+connectDB();
+
+// ✅ Export app for Vercel
 export default app;
 
-// ✅ Local development: run server manually
+// ✅ Local development only
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   mongoose
